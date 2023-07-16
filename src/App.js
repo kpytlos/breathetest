db420317e35c2d0b3ab9da4dcc545e05c4c71a29
kpytlos <pytlos90@gmail.com>
2023-07-16 17:42:48 +0200

    Update app.module.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import "./App.css";
+import React from "react";
+import appStyles from "./App.module.css";
 import CircleAnimation from "./Components/CircleAnimation";
 import videoFile from "./Assets/video1.mp4";
 import InitialScreenSettings from "./Components/InitialScreenSettings";
@@ -7,15 +7,17 @@ import InitialScreenSettings from "./Components/InitialScreenSettings";
 const App = () => {
   const initialScreen = InitialScreenSettings();
   return (
-    <div className="MainAppContainer">
-      <div className="App-header">
-        {initialScreen === "hey" && <p className="firstMessage">Hey.</p>}
+    <div className={appStyles.mainAppContainer}>
+      <div className={appStyles["app-header"]}>
+        {initialScreen === "hey" && (
+          <p className={appStyles.firstMessage}>Hey.</p>
+        )}
         {initialScreen === "justBreathe" && (
-          <p className="firstMessage">Just breathe...</p>
+          <p className={appStyles.firstMessage}>Just breathe...</p>
         )}
         {initialScreen === "circle" && <CircleAnimation />}
       </div>
-      <video autoPlay muted loop id="background-video">
+      <video autoPlay muted loop className={appStyles["background-video"]}>
         <source src={videoFile} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
